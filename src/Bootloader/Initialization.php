e0d19644096d5e42caee40bfe8097c4fe7d9d32e
Leo <losoviz@gmail.com>
2019-05-29 22:21:10 +0800

    Moved saving cache to the component's boot

--- src/Bootloader/Initialization.php
+++ src/Bootloader/Initialization.php
@@ -2,15 +2,11 @@
 namespace PoP\Engine\Bootloader;
 
 use PoP\Root\Component\ComponentManager;
-use PoP\Root\Container\ContainerBuilderFactory;
 
 class Initialization
 {
     public static function init()
     {
-        // Compile and Cache Symfony's DependencyInjection Container Builder
-        ContainerBuilderFactory::maybeCompileAndCacheContainer();
-
         // Boot all the components
         ComponentManager::boot();
 
