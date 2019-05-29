78d85fd02c88a55735aa30e7a7da2311a01da35d
Leo <losoviz@gmail.com>
2019-05-29 11:02:34 +0800

    Enabled default parameters in services.yaml

--- src/Bootloader/Initialization.php
+++ src/Bootloader/Initialization.php
@@ -1,10 +1,21 @@
 <?php
 namespace PoP\Engine\Bootloader;
 
+use PoP\Root\Component\ComponentManager;
+use PoP\Root\Container\ContainerBuilderFactory;
+
 class Initialization
 {
     public static function init()
     {
+        // Compile Symfony's DependencyInjection Container Builder
+        $containerBuilder = ContainerBuilderFactory::getInstance();
+        $containerBuilder->compile();
+
+        // Boot all the components
+        ComponentManager::boot();
+
+        // Instantiate all those immediately-required files
         InstantiateNamespaceClasses::init();
     }
 }
--- src/Component.php
+++ src/Component.php
@@ -1,6 +1,8 @@
 <?php
 namespace PoP\Engine;
 
+// use PoP\Hooks\Facades\HooksAPI;
+use PoP\Root\Component\AbstractComponent;
 use PoP\Root\Component\YAMLServicesTrait;
 use PoP\Root\Component\PHPConfigurableServicesTrait;
 use PoP\Engine\Component\InstantiateNamespaceClassesTrait;
@@ -8,21 +10,40 @@ use PoP\Engine\Component\InstantiateNamespaceClassesTrait;
 /**
  * Class required to check if this component exists and is active
  */
-class Component
+class Component extends AbstractComponent
 {
     use YAMLServicesTrait;
     use PHPConfigurableServicesTrait;
     use InstantiateNamespaceClassesTrait;
+
+    // protected static function initializeServices()
+    // {
+    //     HooksAPI::getInstance()->addAction(
+    //         'popcms:init', 
+    //         array($this, 'init'), 
+    //         PHP_INT_MAX
+    //     );
+    // }
 
     /**
      * Initialize services
      */
     public static function init()
     {
+        parent::init();
         self::initYAMLServices(dirname(__DIR__));
-        self::initPHPServiceConfiguration(dirname(__DIR__));
         self::instantiateNamespaceClasses([
             __NAMESPACE__.'\HookImplementations',
         ]);
+    }
+
+    /**
+     * Initialize Service Configuration
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        self::initPHPServiceConfiguration(dirname(__DIR__));
     }
 }
