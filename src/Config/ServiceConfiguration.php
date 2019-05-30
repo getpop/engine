1816cdaf2488953c11faa2cd822f1f82d3d2935a
Leo <losoviz@gmail.com>
2019-05-30 11:53:36 +0800

    Abstracted code into common function

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -2,9 +2,7 @@
 namespace PoP\Engine\Config;
 
 use PoP\Root\Container\ContainerBuilderUtils;
-use PoP\Root\Container\ContainerBuilderFactory;
 use PoP\Root\Component\PHPServiceConfigurationTrait;
-use Symfony\Component\DependencyInjection\Reference;
 
 class ServiceConfiguration
 {
@@ -12,15 +10,11 @@ class ServiceConfiguration
 
     protected static function configure()
     {
-        $containerBuilder = ContainerBuilderFactory::getInstance();
-        
         // Add ModuleFilter to the ModuleFilterManager
-        $definition = $containerBuilder->getDefinition('module_filter_manager');
-        $moduleFilterServiceIds = ContainerBuilderUtils::getNamespaceServiceIds(
-            'PoP\\Engine\\ModuleFilter\\Implementations'
+        ContainerBuilderUtils::injectServicesIntoService(
+            'module_filter_manager',
+            'PoP\\Engine\\ModuleFilter\\Implementations',
+            'add'
         );
-        foreach ($moduleFilterServiceIds as $moduleFilterServiceId) {
-            $definition->addMethodCall('add', [new Reference($moduleFilterServiceId)]);
-        }
     }
 }
