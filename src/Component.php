a303f2ee70440d553e3dda3072ba71f021b16e8e
Leo <losoviz@gmail.com>
2019-09-11 11:27:16 +0800

    Commented version

--- src/Component.php
+++ src/Component.php
@@ -12,7 +12,7 @@ use PoP\Root\Container\ContainerBuilderUtils;
 class Component extends AbstractComponent
 {
     use YAMLServicesTrait;
-    const VERSION = '0.1.0';
+    // const VERSION = '0.1.0';
 
     /**
      * Initialize services
