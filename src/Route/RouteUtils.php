e8cfddb629855e12afe45b4380c7f30870ba2c11
Leo <losoviz@gmail.com>
2019-10-22 23:09:01 +0800

    Moved getRouteURL/Title to Engine

---
+++ src/Route/RouteUtils.php
@@ -0,0 +1,23 @@
+<?php
+namespace PoP\Engine\Route;
+
+class RouteUtils {
+
+    public static function getRouteURL($route)
+    {
+        // For the route, the ID is the URI applied on the homeURL instead of the domain
+        // (then, the id for domain.com/en/slug/ is "slug" and not "en/slug")
+        $cmsengineapi = \PoP\Engine\FunctionAPIFactory::getInstance();
+        $homeurl = GeneralUtils::maybeAddTrailingSlash($cmsengineapi->getHomeURL());
+        return $homeurl.$route.'/';
+    }
+    public static function getRouteTitle($route)
+    {
+        $title = HooksAPIFacade::getInstance()->applyFilters(
+            'route:title',
+            $route,
+            $route
+        );
+        return $title;
+    }
+}
