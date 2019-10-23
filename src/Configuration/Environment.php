d0940d52c34a3cacb878dd85b00560455c059eab
Leo <losoviz@gmail.com>
2019-10-23 23:38:51 +0800

    Renamed constant

--- src/Configuration/Environment.php
+++ src/Configuration/Environment.php
@@ -5,7 +5,7 @@ class Environment
 {
     public static function disablePersistingDefinitionsOnEachRequest()
     {
-        return isset($_ENV['DISABLE_PERSISTING_DEFINITIONS_EACH_REQUEST']) ? strtolower($_ENV['DISABLE_PERSISTING_DEFINITIONS_EACH_REQUEST']) == "true" : false;
+        return isset($_ENV['DISABLE_PERSISTING_DEFINITIONS_ON_EACH_REQUEST']) ? strtolower($_ENV['DISABLE_PERSISTING_DEFINITIONS_ON_EACH_REQUEST']) == "true" : false;
     }
 }
 
