eecd0279646d77333cc260608d0d7feb3717680e
Leo <losoviz@gmail.com>
2019-12-04 19:27:23 +0800

    Migrated loading DBObject by param to dataloader

---
+++ src/ModuleProcessors/DBObjectIDFromURLParamModuleProcessorTrait.php
@@ -0,0 +1,11 @@
+<?php
+namespace PoP\Engine\ModuleProcessors;
+
+trait DBObjectIDFromURLParamModuleProcessorTrait
+{
+    abstract protected function getDBObjectIDParamName(array $module, array &$props, &$data_properties);
+    protected function getDBObjectIDFromURLParam(array $module, array &$props, &$data_properties)
+    {
+        return $_REQUEST[$this->getDBObjectIDParamName($module, $props, $data_properties)];
+    }
+}
