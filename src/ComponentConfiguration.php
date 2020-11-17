944be1b65be23431cbf7948b27624c7104d3aa2e
Leonardo Losoviz <leo@getpop.org>
2020-11-17 14:05:17 +0800

    Disable redundant mutation fields in the root type

--- src/ComponentConfiguration.php
+++ src/ComponentConfiguration.php
@@ -12,6 +12,7 @@ class ComponentConfiguration
     use ComponentConfigurationTrait;
 
     private static $addMandatoryCacheControlDirective;
+    private static $disableRedundantRootTypeMutationFields;
 
     public static function addMandatoryCacheControlDirective(): bool
     {
@@ -29,6 +30,24 @@ class ComponentConfiguration
             $defaultValue,
             $callback,
             $useHook
+        );
+        return $selfProperty;
+    }
+
+    public static function disableRedundantRootTypeMutationFields(): bool
+    {
+        // Define properties
+        $envVariable = Environment::DISABLE_REDUNDANT_ROOT_TYPE_MUTATION_FIELDS;
+        $selfProperty = &self::$disableRedundantRootTypeMutationFields;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
         );
         return $selfProperty;
     }
