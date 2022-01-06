6bfeab8c75c7f043715aa993f41c3694d5e4fa92
leoloso <leo@getpop.org>
2022-01-06 09:02:03 +0000

    Moved trait from ComponentModel to Engine

---
+++ src/ModuleProcessors/FormMultipleInputModuleProcessorTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\ModuleProcessors;
+
+use PoP\Engine\FormInputs\MultipleInputFormInput;
+
+trait FormMultipleInputModuleProcessorTrait
+{
+    /**
+     * @return string[]
+     */
+    public function getInputSubnames(array $module): array
+    {
+        return [];
+    }
+
+    public function getInputOptions(array $module): array
+    {
+        $options = parent::getInputOptions($module);
+        $options['subnames'] = $this->getInputSubnames($module);
+        return $options;
+    }
+
+    public function getInputClass(array $module): string
+    {
+        return MultipleInputFormInput::class;
+    }
+}
